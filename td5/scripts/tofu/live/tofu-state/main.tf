6f088de5f41acae2834afbf810ee94fd7451850c
ilyasgdo <ilyas.ghandaoui@edu.esiee.fr>
2025-12-24 15:22:23 +0100

    Add CD pipeline: S3 backend, IAM roles for plan/apply, and deployment workflows

--- td5/scripts/tofu/live/tofu-state/main.tf
+++ td5/scripts/tofu/live/tofu-state/main.tf
@@ -5,6 +5,5 @@ provider "aws" {
 module "state" {
   source = "github.com/brikis98/devops-book//ch5/tofu/modules/state-bucket"
 
-  # TODO: fill in your own bucket name!
-  name = "fundamentals-of-devops-tofu-state"
+  name = "tofu-state-118499504231-ilyasgdo"
 }
